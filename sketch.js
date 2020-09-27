6330f6d1eea4440ac22cd00cb0ad2d10ee37f103
MANAV NAYAK <66175257+ManavNayak@users.noreply.github.com>
2020-09-27 21:52:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,13 +16,13 @@ var player_img;
 
 
 function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
+  back_img = loadImage("FRUITS-AGAIN/images/jungle.jpg");
+  player_img = loadImage("FRUITS-AGAIN/images/basket2.png");
+  fruit1_img = loadImage("FRUITS-AGAIN/images/apple2.png");
+  fruit2_img = loadImage("FRUITS-AGAIN/images/banana2.png");
+  fruit3_img = loadImage("FRUITS-AGAIN/images/melon2.png");
+  fruit4_img = loadImage("FRUITS-AGAIN/images/orange2.png");
+  fruit5_img = loadImage("FRUITS-AGAIN/images/pineapple2.png");
   fruitGroup = new Group();
 }
 function setup() {
